df9821e59ccf9d327a3b45516eedf41e49425f93
Kevin <>
2023-06-19 00:28:04 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -101,3 +101,95 @@
     
 })(jQuery);
 
+
+
+// about me box
+fetch('index.html')
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(data, 'text/html');
+    const aboutMeDiv = htmlDoc.getElementById('about_me');
+    const readMoreLink = htmlDoc.getElementById('read_more');
+    readMoreLink.remove(); // Remove unwanted element
+    document.getElementById('about_me_container').appendChild(aboutMeDiv);
+  });
+
+//   footer
+  fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(data, 'text/html');
+    const footerDiv = htmlDoc.getElementById('footer');
+    document.getElementById('footer').appendChild(footerDiv);
+  });
+
+//   Portfolio
+  fetch('portfolio.html')
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(data, 'text/html');
+    const mySkillsDiv = htmlDoc.getElementById('mySkills');
+    document.getElementById('mySkills_1').appendChild(mySkillsDiv);
+  });
+
+//   Img Box
+  var fullImgBox = document.getElementById("fullImgBox");
+  var fullImg = document.getElementById("fullImg");
+
+  function openFullImg(pic) {
+      fullImgBox.style.display = "flex";
+      fullImg.src = pic;
+      image_1.style.display = "flex";
+  }
+
+  function closeFullImg() {
+      fullImgBox.style.display = "none";
+  };
+
+  let imgBx = document.querySelectorAll('.imgBx');
+  imgBx.forEach(popup => popup.addEventListener('click', () => {
+      popup.classList.toggle('active')
+  }))
+
+//   Carousel
+const carousel = document.querySelector('.gallery-carousel');
+const slides = carousel.querySelector('.carousel-slides');
+const dots = carousel.querySelector('.carousel-dots');
+const images = slides.querySelectorAll('img');
+const dotArray = [];
+
+let index = 0;
+
+function showImage() {
+  images.forEach(image => image.classList.remove('active'));
+  images[index].classList.add('active');
+  
+  dotArray.forEach(dot => dot.classList.remove('active'));
+  dotArray[index].classList.add('active');
+
+  index = (index + 1) % images.length;
+}
+
+function createDots() {
+  for (let i = 0; i < images.length; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('carousel-dot');
+    dot.addEventListener('click', () => {
+      index = i;
+      showImage();
+    });
+    dots.appendChild(dot);
+    dotArray.push(dot);
+  }
+  dotArray[0].classList.add('active');
+}
+
+createDots();
+
+showImage(); // Show the first image immediately
+
+setInterval(showImage, 5000); // Show the next image every 5 seconds
+
